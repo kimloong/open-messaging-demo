76ac69b919ea476ee49c34f1b04cfa711d1f9f18
Kimloong <312891416@qq.com>
2017-07-02 23:00:14 +0800

    v3:拆分文件，每个文件固定在128M

--- src/main/java/io/openmessaging/demo/MessageStore.java
+++ src/main/java/io/openmessaging/demo/MessageStore.java
@@ -2,31 +2,36 @@ package io.openmessaging.demo;
 
 import io.openmessaging.KeyValue;
 import io.openmessaging.Message;
-import io.openmessaging.Producer;
-import io.openmessaging.PullConsumer;
 import io.openmessaging.exception.OMSRuntimeException;
 
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.LinkedBlockingDeque;
 
 public class MessageStore {
 
     private static final MessageStore INSTANCE = new MessageStore();
 
+    private static final int MAX_FILE_SIZE = 128 * 1024 * 1024;
     private static final int BYTE_BUFFER_POOL_SIZE = 20;
     private static final int META_SIZE = 4;
     private static final int MAX_MESSAGE_SIZE = 256 * 1024;
 
     private final Map<String, RandomAccessFile> writeAccessMap = new HashMap<>();
+    private final Map<String, Object> writeLocks = new HashMap<>();
+    private final Map<String, Integer> writeIndexes = new HashMap<>();
 
-    private final Map<String, RandomAccessFile> readAccessMap = new ConcurrentHashMap<>();
+    private final Map<String, RandomAccessFile> readAccessMap = new HashMap<>();
+    private final Map<String, Object> readLocks = new HashMap<>();
+    private final Map<String, Integer> readIndexes = new HashMap<>();
+
+
     private final BlockingQueue<ByteBuffer> byteBufferPool = new LinkedBlockingDeque<>(BYTE_BUFFER_POOL_SIZE);
     private String storePath;
     private OMSSerializer serializer = new OMSCustomSerializer();
@@ -47,29 +52,17 @@ public class MessageStore {
         FileChannel writeChannel;
         ByteBuffer writeByteBuffer = null;
         try {
-            writeAccess = writeAccessMap.get(bucket);
-            if (null == writeAccess) {
-                synchronized (writeAccessMap) {
-                    writeAccess = writeAccessMap.get(bucket);
-                    if (null == writeAccess) {
-                        File file = new File(storePath, bucket);
-                        writeAccess = new RandomAccessFile(file, "rw");
-                        if (!file.createNewFile()) {
-                            writeAccess.seek(file.length());
-                        }
-                        writeAccessMap.put(bucket, writeAccess);
-                    }
-                }
-            }
-
-
             writeByteBuffer = byteBufferPool.take();
             writeByteBuffer.clear();
-            serializer.serializeMessage(message,writeByteBuffer);
+            serializer.serializeMessage(message, writeByteBuffer);
 
-            synchronized (writeAccess) {
+            Object lock = getWriteLock(bucket);
+
+            synchronized (lock) {
+                writeAccess = getWriteAccess(bucket);
                 writeChannel = writeAccess.getChannel();
-
+                if (writeChannel.position() >= MAX_FILE_SIZE) {
+                }
                 writeChannel.write(writeByteBuffer);
             }
         } catch (IOException | InterruptedException e) {
@@ -87,22 +80,12 @@ public class MessageStore {
         FileChannel readChannel = null;
         ByteBuffer readByteBuffer = null;
         try {
-            String key = queue + "@" + bucket;
-            readAccess = readAccessMap.get(key);
-            if (null == readAccess) {
-                synchronized (readAccessMap) {
-                    readAccess = readAccessMap.get(key);
-                    if (null == readAccess) {
-                        File file = new File(storePath, bucket);
-                        if (!file.exists()) {
-                            return null;
-                        }
-                        readAccess = new RandomAccessFile(file, "r");
-                        readAccessMap.put(key, readAccess);
-                    }
+            Object lock = getReadLock(queue, bucket);
+            synchronized (lock) {
+                readAccess = getReadAccess(queue, bucket);
+                if (readAccess == null) {
+                    return null;
                 }
-            }
-            synchronized (readAccess) {
                 readChannel = readAccess.getChannel();
 
                 readByteBuffer = byteBufferPool.take();
@@ -130,5 +113,102 @@ public class MessageStore {
                 byteBufferPool.add(readByteBuffer);
             }
         }
+    }
+
+    private Object getWriteLock(String bucket) {
+        return getLock(bucket, writeLocks);
+    }
+
+    private Object getReadLock(String queue, String bucket) {
+        String key = queue + "@" + bucket;
+        return getLock(key, readLocks);
+    }
+
+    private Object getLock(String key, Map<String, Object> locks) {
+        Object lock = locks.get(key);
+        if (null == lock) {
+            synchronized (locks) {
+                lock = locks.get(key);
+                if (null == lock) {
+                    lock = new Object();
+                    locks.put(key, lock);
+                }
+            }
+        }
+        return lock;
+    }
+
+    private RandomAccessFile getWriteAccess(String bucket) throws IOException {
+        RandomAccessFile writeAccess;
+        writeAccess = writeAccessMap.get(bucket);
+        if (null == writeAccess) {
+            Integer index = writeIndexes.get(bucket);
+            if (null == index) {
+                index = 1;
+                writeIndexes.put(bucket, index);
+            }
+
+            File file = new File(storePath, bucket + "-" + index);
+            writeAccess = new RandomAccessFile(file, "rw");
+            if (!file.createNewFile()) {
+                writeAccess.seek(file.length());
+            }
+            writeAccessMap.put(bucket, writeAccess);
+        } else {
+            FileChannel fileChannel = writeAccess.getChannel();
+            if (fileChannel.position() >= MAX_FILE_SIZE) {
+                Integer index = writeIndexes.get(bucket);
+                index++;
+                writeIndexes.put(bucket, index);
+
+                fileChannel.close();
+                writeAccess.close();
+
+                File file = new File(storePath, bucket + "-" + index);
+                writeAccess = new RandomAccessFile(file, "rw");
+                if (!file.createNewFile()) {
+                    writeAccess.seek(file.length());
+                }
+                writeAccessMap.put(bucket, writeAccess);
+            }
+        }
+        return writeAccess;
+    }
+
+    private RandomAccessFile getReadAccess(String queue, String bucket) throws IOException {
+        RandomAccessFile readAccess;
+        String key = queue + "@" + bucket;
+        readAccess = readAccessMap.get(key);
+        if (null == readAccess) {
+            Integer index = readIndexes.get(key);
+            if (null == index) {
+                index = 1;
+                readIndexes.put(key, index);
+            }
+
+            File file = new File(storePath, bucket + "-" + index);
+            if (!file.exists()) {
+                return null;
+            }
+            readAccess = new RandomAccessFile(file, "r");
+            readAccessMap.put(key, readAccess);
+        } else {
+            FileChannel fileChannel = readAccess.getChannel();
+            if (fileChannel.position() >= MAX_FILE_SIZE) {
+                Integer index = readIndexes.get(key);
+                index++;
+                readIndexes.put(key, index);
+
+                File file = new File(storePath, bucket + "-" + index);
+                if (!file.exists()) {
+                    return null;
+                }
+                fileChannel.close();
+                readAccess.close();
+                readAccess = new RandomAccessFile(file, "r");
+                readAccessMap.put(key, readAccess);
+            }
+        }
+        return readAccess;
     }
 }
